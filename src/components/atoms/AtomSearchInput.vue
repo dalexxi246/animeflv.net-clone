0e00a28170810730f0a6b46138edf8a2f7552d4c
Wilmer Alexis Hurtado Hurtado <whurtado@merqueo.com>
2021-05-15 19:41:43 -0500

    Stylish search box component

--- src/components/atoms/AtomSearchInput.vue
+++ src/components/atoms/AtomSearchInput.vue
@@ -1,8 +1,8 @@
 <template>
   <b-nav-form>
-    <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+    <b-form-input size="sm" class="mr-sm-2" placeholder="Buscar" />
     <b-button size="sm" class="my-2 my-sm-0" type="submit">
-      <i class="fas fa-camera"></i>
+      <b-icon icon="search" variant="light"/>
     </b-button>
   </b-nav-form>
 </template>
@@ -19,4 +19,26 @@ export default {
 </script>
 
 <style scoped>
+button {
+  background: none;
+  border: none;
+}
+input {
+  background: none;
+  border: none;
+  color: white;
+}
+input:focus {
+  background: none;
+  border: none;
+  color: white;
+}
+input::placeholder {
+  color: white;
+}
+form {
+  border-radius: 25px;
+  background: #252a2e;
+  padding: 2px 8px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
-import Vue from 'vue'
 import './plugins/bootstrap-vue'
+
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import { BootstrapVueIcons, BootstrapVue } from "bootstrap-vue";
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
